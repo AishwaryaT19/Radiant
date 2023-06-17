4463035ceba39c8c6f8a904fe54de30df1e71a67
AishwaryaT19 <aishwarya.tewari19@gmail.com>
2023-06-17 22:05:41 +0530

    init

---
+++ pages/terms-of-service.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function termsOfService() {
+  return (
+    <>
+      <Head>
+        <title>Terms of Service</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="refresh" content="0; URL=/termsofservice.pdf" />
+      </Head>
+      <p>Terms of Service</p>
+    </>
+  );
+}
